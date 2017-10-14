0500dc1038abe7e9c11d90b7c39deb248396e985
Leix_b <abone9999@gmail.com>
2017-10-14 12:45:59 +0200

    Added city parser

--- python/parser_cities.py
+++ python/parser_cities.py
@@ -1,7 +1,22 @@
+#!/usr/bin/env python3
 import json
 
-with open('./cities.json', 'r') as json_file:
-    json_data = json.loads(json_file.read())
+with open('cities.json', 'r') as json_file:
+    json_data = json.load(json_file)
 
-for it,shit in json_data.items():
-    print(it)
+# for continent in json_data['Continents']:
+    # for it,shit in continent.items():
+        # if it == 'Name':
+            # print(it, shit)
+
+europe = json_data['Continents'][2]
+
+cities = {}
+
+for country in europe['Countries']:
+    for city in country['Cities']:
+        cities[city['Name']] = dict(
+                id=city['Id'],
+                location=tuple(city['Location'].split(' '))
+                )
+
